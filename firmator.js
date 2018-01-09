96066a3851e9a4f1786ae02d215764db434ad1df
Puldzian <puldzian@gmail.com>
2018-01-09 20:47:57 +0100

    to jest wszystko pojebane

--- firmator.js
+++ firmator.js
@@ -1,6 +1,5 @@
 /*global $, document, branze, setTimeout, imionaZenskie, koncowkaZenska, poczatek, srodek, koncowka */
 
-// Dlugosc zbioru branz
 var BRANZE_N = branze.length,
     IMIONAZEN_N = imionaZenskie.length,
     KONCOWKAZEN_N = koncowkaZenska.length,
@@ -28,32 +27,36 @@ var generatorBranzy = function (seed) {
     }
 };
 
-// Korektor
-var korektor = function (problem) {
+// Sprawdź czy jest dubel i usuń (ale tylko pierwszy)
+var usunDubel = function (slowo) {
     "use strict";
-    var slowo = problem,
+    var noweSlowo,
         a,
         b,
-        dd = slowo.length,
-        i;
-    for (i = 0; i < dd; i += 1) {
+        i; //JSLint to chuj
+    for (i = 0; i < slowo.length; i += 1) {
         b = slowo[i];
         if (i > 0) {
             a = slowo[i - 1];
         }
         // Czy litery są tożsame?
         if (a === b) {
-            alert("Slowo[a] to " + slowo[i] + ", a slowo[b] to " + slowo[i - 1]);
-            // problem = string, więc splice nie działa
-            slowo[i] = "C";
-            // Rekalkuluj długość
-            dd = slowo.length;
+            // alert("Slowo[a] to " + slowo[i] + ", a slowo[b] to " + slowo[i - 1]);
+            var ab = a + b;
+            noweSlowo = slowo.replace(ab, a);
         }
     }
-    return slowo;
+    return noweSlowo;
 };
 
-// Imię żeńskie z końcowką
+// Korektor
+var korektor = function (slowo) {
+    "use strict";
+    var noweSlowo = usunDubel(slowo);
+    return noweSlowo;
+};
+
+/* Imię żeńskie z końcowką
 var silnik1 = function () {
     "use strict";
     var nr1 = losuj(IMIONAZEN_N),
@@ -62,7 +65,7 @@ var silnik1 = function () {
         firma2 = koncowkaZenska[nr2];
     $("#jsFirma1").html(firma1);
     $("#jsFirma2").html(firma2);
-};
+}; */
 
 // Klasyczny, duży silnik
 var silnik2 = function () {
@@ -75,23 +78,25 @@ var silnik2 = function () {
         firma3 = koncowka[nr3],
         firma123 = firma1 + firma2 + firma3;
     // UWAGA UWAGA, powinien byc (firma123)
-    firma123 = korektor("MIGAAGIM");
+    var firma456 = korektor(firma123);
     $("#jsFirma1").html(firma123);
 };
 
 // Generator firm
 var nowyGenerator = function () {
     "use strict";
-    var nrSilnika;
+    silnik2();
+    /*
+    var nrSilnika = 1;
     if (trybGeneratora === 0) {
         // Tutaj będzie liczba dostępnych silników
         nrSilnika = 0;
     } else {
         // Albo silnik po prostu brany z radioboksa
         nrSilnika = trybGeneratora;
-    }
+    } */
     // Tutaj następuje właściwy wybór silnika
-    switch (nrSilnika) {
+    /* switch (nrSilnika) {
     case 0:
         silnik1();
         break;
@@ -100,7 +105,7 @@ var nowyGenerator = function () {
         break;
     default:
         $("#jsFirma1").html("ZEPSUŁO SIĘ");
-    }
+    } */
 };
 
 // Odświeżanie ekranu i przejazd przez wszystko
